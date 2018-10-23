944eb750d00e14cd1779e1d4fdebad510db40f28
deamaulidya <maulidyadea@gmail.com>
2018-10-23 22:37:55 +0700

    first commit

---
+++ src/HelloWorld1.java
@@ -0,0 +1,18 @@
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("lala\nla ");
+        System.out.print("hihihi");
+        System.out.println();
+        first();
+        newmain();
+    }
+    public static void first(){
+        int x=90;
+        System.out.println("kanan "+x);
+    }
+
+    public static void newmain(){
+        System.out.println("hai hai");
+    }
+}
